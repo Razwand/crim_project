6d7b98176551a416da02724bbab3cf848f361817
alisrod <sanzrodal@gmail.com>
2021-06-08 12:47:51 +0200

    Comment script

--- Code/Scrapping/scrap_img.py
+++ Code/Scrapping/scrap_img.py
@@ -1,11 +1,29 @@
+#!/usr/bin/env python
+
+"""scrap_img.py: This script takes de web criminalia and scrapps images from all the listed profiles. 
+Images can be downloaded choosing between man/woman profiles andthe folder where they will be stored."""
+
+__author__ = "Alicia Sánchez R."
+
 import requests
 from bs4 import BeautifulSoup
 import urllib
 import string
 from os import path
 
-
 def read_sub_page(url_m, path_folder_img):
+
+    '''
+    From the single profile url:
+    TRY:
+    - Read html looking for container id
+    - Filter by 'image' class
+    - find 'img'
+    - Save image in selected path with name refering to the profile name
+    IF NOT:
+    - Img saving has not been possible and print out the name of the profile that hasn't been scrapped
+
+    '''
 
     try:
         subpage = requests.get(url_m)
@@ -19,6 +37,15 @@ def read_sub_page(url_m, path_folder_img):
         print('NOT SAVED: ',url_m)
 
 def read_details(job_el,path_folder_img):
+
+    '''
+    For each element (from index 1 as first position stores the table tittle):
+    - Name extraction
+    - Look for 'more' class
+    - href filter to take url from single profile
+    - read page extracted in the previous step
+
+    '''
     
     for i in range(1,len(job_el)):
         name_murder = job_el[i].find('div', class_='name')
@@ -38,6 +65,15 @@ def read_murder_browser(gender_selected,letter,country):
     return(url)
 
 def main():
+
+    '''
+    Main flow starting with parameter 'Gender' and path where the images will be stored.
+    Letter by letter the following steps will be taken:
+    - Build url searching by gender & letter
+    - Parse html filtering by id=content
+    - Extract all block elements in a list job_elems
+    - read details from the table of all elements of the page search.
+    '''
     
     gender_selected = input('Gender (hombre o mujer): ')
     while gender_selected not in ['hombre','mujer']:
